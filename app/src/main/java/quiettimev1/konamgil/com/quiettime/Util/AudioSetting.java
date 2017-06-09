a515aade74961534c507aa991bb726a0bcb7c7b4
chohana <hana@letscombine.com>
2017-06-09 18:34:11 +0900

    테스트중2

--- app/src/main/java/quiettimev1/konamgil/com/quiettime/Util/AudioSetting.java
+++ app/src/main/java/quiettimev1/konamgil/com/quiettime/Util/AudioSetting.java
@@ -73,13 +73,13 @@ public class AudioSetting {
      * 벨소리 최대볼륨의 절반으로 만듭니다
      */
     public void setUpAudioVolume(){
+        int maxVoulume = mAlramMAnager.getStreamMaxVolume(AudioManager.STREAM_RING);
+        mAlramMAnager.setStreamVolume(AudioManager.STREAM_RING, maxVoulume/2, AudioManager.FLAG_PLAY_SOUND);
+        mAlramMAnager.setStreamVolume(AudioManager.STREAM_NOTIFICATION, maxVoulume/2, AudioManager.FLAG_PLAY_SOUND);
+        mAlramMAnager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVoulume/2, AudioManager.FLAG_PLAY_SOUND);
+        mAlramMAnager.setStreamVolume(AudioManager.STREAM_SYSTEM, maxVoulume/2, AudioManager.FLAG_PLAY_SOUND);
+        mAlramMAnager.setStreamVolume(AudioManager.STREAM_ALARM, maxVoulume/2, AudioManager.FLAG_PLAY_SOUND);
         mAlramMAnager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
-//        int maxVoulume = mAlramMAnager.getStreamMaxVolume(AudioManager.STREAM_RING);
-//        mAlramMAnager.setStreamVolume(AudioManager.STREAM_RING, maxVoulume/2, AudioManager.FLAG_PLAY_SOUND);
-//        mAlramMAnager.setStreamVolume(AudioManager.STREAM_NOTIFICATION, maxVoulume/2, AudioManager.FLAG_PLAY_SOUND);
-//        mAlramMAnager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVoulume/2, AudioManager.FLAG_PLAY_SOUND);
-//        mAlramMAnager.setStreamVolume(AudioManager.STREAM_SYSTEM, maxVoulume/2, AudioManager.FLAG_PLAY_SOUND);
-//        mAlramMAnager.setStreamVolume(AudioManager.STREAM_ALARM, maxVoulume/2, AudioManager.FLAG_PLAY_SOUND);
     }
 
     /**
